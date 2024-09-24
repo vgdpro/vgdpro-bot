6201e3866077711c9b8a59221bcbf73912a3505b
jwyxym <jwyxym@126.com>
2024-09-24 18:11:38 +0800

    Update index.ts

--- vgpro-bot/src/index.ts
+++ vgpro-bot/src/index.ts
@@ -18,7 +18,8 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 const message = [
   'VGPro是一款由VGPro制作组自主研发的一款全新开放世界打牌游戏，游戏发生在一个被称作「卡片战斗先导者」的幻想牌桌，在这里，被命运选中的人将被授予「命运者卡片」，获得挑战命运的机会。你将扮演被「命运者卡片」选中的其中一人，以先导者对战，争夺改变命运的力量——同时，逐步发掘「命运大战」的真相。',
-  '你好，我是VGProject官方机器人，你可以使用/sc命令搜索卡片，或使用/rc命令随机抽出卡池中的一张卡。'
+  '你好，我是VGProject官方机器人，你可以使用/sc命令搜索卡片，或使用/rc命令随机抽出卡池中的一张卡。',
+  '欢迎加入VGPro官方用户群，在这里，你可以询问一切关于VGPro的东西'
 ]
 
 const cards_json = JSON.parse(readFileSync('./external/cards.json', 'utf8'));
@@ -50,7 +51,7 @@ export function apply(ctx: Context) {
     session.send((0, koishi.h)('message', message[1]));
   })
   ctx.command("vgpro用户群").action(({session})=>{
-    // let url = 
+     let url = ''//待填入
     session.send((0, koishi.h)('message', message[2], (0, koishi.h)('image', { url: url })));
   })
 }
